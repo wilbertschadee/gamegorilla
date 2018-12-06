a1dd82f5a2676fe106aa0419d53ce350926a5599
wilbert <wilbert_schadee_91@hotmail.com>
2018-12-06 13:21:59 +0100

    changed cart.php

--- cart.php
+++ cart.php
@@ -1,54 +1,19 @@
-<?php
+<?php include "nav.php" ?>
 
-    header("Content-Type: text/html; charset=ISO-8859-1");
-
-    include "php/dbConnection.php";
-
-    session_start();
-
-    if(empty($_SESSION['cart'])){
-        echo 'jou winkel mandje is leeg';
-    }else{
-
-        $productID = implode(',', $_SESSION['cart']);
-
-        // echo $productID;
-
-        $sql = "SELECT * FROM products Where id IN ($productID)";
-
-        $data = $conn->query($sql);
-
-        // echo $sql;
-        include "nav.php";
- echo '<div class="wrapper">';
-
-        foreach($data as $row){
-           
-                echo'<div class="cart">
-                <div class="cartProduct">
-                    <img src="'. $row['img_path'] . '" alt="">
-                    <div class="cartInfo">
-                        <div class="cartInfoName">
-                        <h1 class="name">' . $row['name'] . '</h1>'
-                        .($row["sale"] == 1?'<p class="productPrice"><span class="oldPrice">&euro; ' . $row['price'] . '</span> - &euro;' . $row['sale_price'] . '</p>':'<div class="productPrice">&euro;'. $row['price'] . '</div>').
-                        '</div>
-                        <div class="cartInfoDes">'
-                            .$row['description'].
-                        '</div>
+    <div class="wrapper">
+        <div class="cart">
+           <?php include "php/showCart.php"; ?>
+                <!-- <div class="money">
+                    <div class="amount">
+                        1
                     </div>
-                    
-                </div>
-            </div>';
-        
-        }
-        echo '</div>';
-    }
-?>
-<!-- <div class="money">
-                        <div class="amount">
-                            1
-                        </div>
-                        <div class="total">
-                            20euro
-                        </div>
-                    </div> -->
+                    <div class="total">
+                        20euro
+                    </div>
+                </div> -->
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
