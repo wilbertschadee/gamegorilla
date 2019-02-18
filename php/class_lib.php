edc5fe34e48cde090e5f780e942fb96cd1fa67d1
Wilbert Schadee <wilbert_schadee_91@hotmail.com>
2019-02-18 11:48:17 +0100

    converting to oop

--- php/class_lib.php
+++ php/class_lib.php
@@ -51,29 +51,90 @@
         private $product_img;
         private $date;
     
-            public function __construct($id, $name, $price, $platform, $sale, $salePrice, $release, $img) {
-                $this -> product_id = $id;
-                $this -> product_name = $name;
-                $this -> product_price = $price;
-                $this -> product_platform = $platform;
-                $this -> product_sale = $sale;
-                $this -> product_salePrice = $salePrice;
-                $this -> product_release = $release;
-                $this -> product_img = $img;
-                $this -> date = date('Y-m-d');
-            }
-    
-            public function echoCard(){
-                echo '<div class="product"> 
-                <a href="' . 'products.php?id=' . $this -> product_id . '">
-                <img src="' . $this -> product_img . '" alt="' . $this -> product_name . '" style="max-width:100%">
-                '.($this -> product_release >= $this -> date?'<p class=release>' . $this -> product_release . ' beschikbaar</p>':"").
-                '<h1 class=name>'. $this -> product_name . ' - ' . $this -> product_platform . '</h1>
-                '.($this -> product_sale == 1?'<p class="price"><span class="oldPrice"> € ' . $this -> product_price . '</span> - €' . $this -> product_salePrice . '</p>':'<p class="price"> € ' . $this -> product_price . '</p>').
-                '</a>
-                </div>';
-            }
+        public function __construct($id, $name, $price, $platform, $sale, $salePrice, $release, $img) {
+            $this -> product_id = $id;
+            $this -> product_name = $name;
+            $this -> product_price = $price;
+            $this -> product_platform = $platform;
+            $this -> product_sale = $sale;
+            $this -> product_salePrice = $salePrice;
+            $this -> product_release = $release;
+            $this -> product_img = $img;
+            $this -> date = date('Y-m-d');
         }
+
+        public function echoCard(){
+            echo '<div class="product"> 
+            <a href="' . 'products.php?id=' . $this -> product_id . '">
+            <img src="' . $this -> product_img . '" alt="' . $this -> product_name . '" style="max-width:100%">
+            '.($this -> product_release >= $this -> date?'<p class=release>' . $this -> product_release . ' beschikbaar</p>':"").
+            '<h1 class=name>'. $this -> product_name . ' - ' . $this -> product_platform . '</h1>
+            '.($this -> product_sale == 1?'<p class="price"><span class="oldPrice"> € ' . $this -> product_price . '</span> - €' . $this -> product_salePrice . '</p>':'<p class="price"> € ' . $this -> product_price . '</p>').
+            '</a>
+            </div>';
+        }
+    }
+
+    class products{
+        private $product_id;
+        private $product_name;
+        private $product_description;
+        private $product_price;
+        private $product_platform;
+        private $product_sale;
+        private $product_salePrice;
+        private $product_release;
+        private $product_img;
+        private $date;
+
+        public function __construct($id, $name, $description, $price, $platform, $sale, $salePrice, $release, $img) {
+            $this -> product_id = $id;
+            $this -> product_name = $name;
+            $this -> product_description = $description;
+            $this -> product_price = $price;
+            $this -> product_platform = $platform;
+            $this -> product_sale = $sale;
+            $this -> product_salePrice = $salePrice;
+            $this -> product_release = $release;
+            $this -> product_img = $img;
+            $this -> date = date('Y-m-d');
+        }
+
+        public function echoProduct(){
+            echo '<div class="productContent">
+            <div class="productRow">
+            <img src="' . $this -> product_img . '" alt="">
+            <div class="productInfo">
+            <h1 class="productName">' . $this -> product_name . ' - ' . $this -> product_platform  . '</h1>'
+            .($this -> product_release >= $this -> date?'<div class="release"> Beschikbaar vanaf ' . $this -> product_release . '</div>':"")
+            .($this -> product_sale == 1?'<p class="productPrice"><span class="oldPrice">&euro; ' . $this -> product_price . '</span> - &euro;' . $this -> product_salePrice . '</p>':'<div class="productPrice">&euro;'. $this -> product_price . '</div>').
+            '<button class="cartBtn" onclick="addToWishlist()">Aan Wishlist toevoegen</button><br><br><br>
+            <button class="cartBtn" onclick="addToCart()">Toevoegen aan winkemandje</button><br><br><br>
+            <div id="feedBack" style="display:none;"></div>'
+            . (isset($_SESSION['role']) && $_SESSION['role'] == 1? '
+            <div>
+            <form action="php/addSale.php?id=' . $this -> product_id . '" method="post">
+            <label for="sale">Sale</label><br>
+            <input class="radio" type="radio" value="1" name="sale"> Ja 
+            <input class="radio" type="radio" value="0" name="sale"> Nee<br>
+            <label for="saleprice">Prijs in de Sale</label>
+            <input class="change" type="text" placeholder="prijs in de Sale" name="saleprice">
+            <button type="submit" class="signupbtn">aanpassen</button> 
+            </form>
+            </div>
+            ':"") .         
+            '</div>
+            <div id="product" style="display:none;">' . $this -> product_id . '</div>
+            </div>
+            <div class="description">Beschrijving</div>
+            <div class="productDescription">'
+            . $this -> product_description . 
+            '</div>
+            </div>';
+    }
+
+
+    }
     
 
 ?>
--- products.php
+++ products.php
@@ -2,49 +2,21 @@
 
     header("Content-Type: text/html; charset=ISO-8859-1");
 
-    include "php/dbConnection.php";
+    include "nav.php";
+    include "php/class_lib.php";
 
     $products = $_GET['id'];
 
-    $sql = "SELECT * FROM products WHERE id = '$products'";
     
-    $data = $conn->query($sql); 
+    $db_conn = new DBC;
+    $db_conn -> init_connection();
 
-    include "nav.php";
-    $date = date('Y-m-d');
+    $getDBData = new getDBData($db_conn -> database_conn, "SELECT * FROM products WHERE id = '$products'");
+    $dbData = $getDBData -> getDBData();
 
-    foreach ($data as $row){
-         
-        echo '<div class="productContent">
-        <div class="productRow">
-        <img src="' . $row['img_path'] . '" alt="">
-        <div class="productInfo">
-        <h1 class="productName">' . $row['name'] . ' - ' . $row['platform'] . '</h1>'
-        .($row["releases"] >= $date?'<div class="release"> Beschikbaar vanaf ' . $row['releases'] . '</div>':"")
-        .($row["sale"] == 1?'<p class="productPrice"><span class="oldPrice">&euro; ' . $row['price'] . '</span> - &euro;' . $row['sale_price'] . '</p>':'<div class="productPrice">&euro;'. $row['price'] . '</div>').
-        '<button class="cartBtn" onclick="addToWishlist()">Aan Wishlist toevoegen</button><br><br><br>
-        <button class="cartBtn" onclick="addToCart()">Toevoegen aan winkemandje</button><br><br><br>
-        <div id="feedBack" style="display:none;"></div'
-        . ($_SESSION['role'] == 1? '
-        <div>
-        <form action="php/addSale.php?id=' . $products . '" method="post">
-        <label for="sale">Sale</label><br>
-        <input class="radio" type="radio" value="1" name="sale"> Ja 
-        <input class="radio" type="radio" value="0" name="sale"> Nee<br>
-        <label for="saleprice">Prijs in de Sale</label>
-        <input class="change" type="text" placeholder="prijs in de Sale" name="saleprice">
-        <button type="submit" class="signupbtn">aanpassen</button> 
-        </form>
-        </div>
-        ':"") .         
-        '</div>
-        <div id="product" style="display:none;">' . $row['id']. '</div>
-        </div>
-        <div class="description">Beschrijving</div>
-        <div class="productDescription">'
-        . $row['description'] . 
-        '</div>
-        </div>';
+    foreach ($dbData as $row){ 
+        $products0 = new products($row['id'], $row['name'], $row['description'], $row['price'], $row['platform'], $row['sale'], $row['sale_price'], $row['releases'], $row['img_path']);
+        $products0 -> echoProduct(); 
     }
 ?>
         
